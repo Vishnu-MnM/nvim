44fc96a229e77874565355badeee44ab507b6c61
Vishnu-MnM <sunny_4210@proton.me>
2026-06-28 21:38:51 -0400

    added HTML, CSS and TS language server configs to lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -63,6 +63,12 @@ vim.lsp.config("ty", {})
 vim.lsp.config("jdtls", {})
 vim.lsp.config("ts_ls", {})
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.lsp.config("html", { capabilities = capabilities })
+vim.lsp.config("cssls", { capabilities = capabilities })
+
 vim.lsp.enable({
     "lua_ls",
     "ruff",
@@ -71,4 +77,6 @@ vim.lsp.enable({
     "gopls",
     "jdtls",
     "ts_ls",
+    "html",
+    "cssls",
 })
